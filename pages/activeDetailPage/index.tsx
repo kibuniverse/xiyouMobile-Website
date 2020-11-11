8b22e39a014b8fcb3c82e2a645036454494f0922
kibuniverse <3050540005@qq.com>
2020-11-11 22:27:47 +0800

    chore: 修改d请求方式

--- pages/activeDetailPage/index.tsx
+++ pages/activeDetailPage/index.tsx
@@ -1,27 +1,15 @@
 
 import axios from 'axios'
+import activityApi from '../../public/api/activity'
 import { useEffect } from 'react'
 import HeaderNav from '../../components/HeaderNav'
-import { GetStaticProps } from 'next'
+import { InferGetStaticPropsType } from 'next'
 
-export default function ActivePageIndex({ context, res }) {
-    console.log(context)
-    console.log(res)
+export default function ActivePageIndex({ posts }) {
+    
     return (
         <div>
             <HeaderNav />
         </div>
     )
-}
-
-export const getStaticProps: GetStaticProps = async (context) => {
-    console.log(context)
-    await axios.get('/api/api/activity/detail/181').then(reslove => {
-        
-    })
-    return {
-        props: {
-            context,
-        }
-    }
 }
